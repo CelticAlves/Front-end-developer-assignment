9a0e1a5a042b514fc5d0e184f1a1605010884359
Celtic Alves <5.fashion@gmail.com>
2019-09-04 18:52:26 +0200

    fixed packages
    fix packages

--- star-wars-app/src/index.js
+++ star-wars-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import * as Sentry from '@sentry/browser';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
